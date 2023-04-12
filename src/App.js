e3bed9aeb60839170a5974b898fbc8c9c823a4b6
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2023-04-12 08:29:50 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Meme from "./components/Meme"
+import "./App.css"
+
+
+
+export default function App() {
+    return (
+        <div>
+        <Navbar/>
+        <Meme/>
+        </div>
+    )
+}
